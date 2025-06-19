fd2c4f7c6c8c18e1f2cd4d72704fdb7be732d2ff
Rub28 <ruhernandezm@gmail.com>
2025-06-19 11:52:25 -0600

    actualizo config.js

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     mysql:{
         host: process.env.MYSQL_HOST || '82.197.82.77',
-        user: process.env.MYSQL_USER || 'u785010228_pventa',
+        user: process.env.MYSQL_USER || 'u785010228_root',
         password: process.env.MYSQL_PASSWORD || 'rub11H28',
         database: process.env.MYSQL_DB || 'u785010228_pventa', 
         port: process.env.PORT_BD || 3306  
