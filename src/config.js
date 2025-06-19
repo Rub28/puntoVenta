48e6539fb54c1e8b94ac50c59891a2ae2cb55ae5
Rub28 <ruhernandezm@gmail.com>
2025-06-18 20:37:44 -0600

    actualizo config.js

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ module.exports = {
         secret: process.env.JET_SECRET || 'miconstraseña2848330'
     },
     mysql:{
-        host: process.env.MYSQL_HOST || '82.29.198.42',
+        host: process.env.MYSQL_HOST || '82.197.82.77',
         user: process.env.MYSQL_USER || 'u785010228_pventa',
         password: process.env.MYSQL_PASSWORD || 'rub11H28',
         database: process.env.MYSQL_DB || 'u785010228_pventa', 
