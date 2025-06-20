b2ce88f7c3283b882ece4b7fe5addc678e76b525
Rub28 <ruhernandezm@gmail.com>
2025-06-19 19:19:43 -0600

    actualizo app.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 
-require('dotenv')
+require('dotenv') 
+/* 
 module.exports = {
     app: {
         port: process.env.PORT || 4001
@@ -15,4 +16,20 @@ module.exports = {
         port: process.env.PORT_BD || 3306  
     }   
 }     
-  
+*/ 
+
+module.exports = {
+    app: {
+        port: process.env.PORT || 4003
+    },
+    jwt:{
+        secret: process.env.JET_SECRET || 'miconstraseña2848330'
+    },
+    mysql:{
+        host: process.env.MYSQL_HOST || '127.0.0.1',
+        user: process.env.MYSQL_USER || 'u785010228_root',
+        password: process.env.MYSQL_PASSWORD || 'rub11H28',
+        database: process.env.MYSQL_DB || 'u785010228_pventa', 
+        port: process.env.PORT_BD || 3306  
+    }   
+}         
