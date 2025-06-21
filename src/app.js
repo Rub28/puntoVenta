7a6c5afc920d1d13390ef14d1148fadcfb629fce
Rub28 <ruhernandezm@gmail.com>
2025-06-20 18:40:28 -0600

    actualizo app.js

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ const options = {
   };
   
   // Crear el servidor HTTPS
-  https.createServer(options, app).listen(4006, () => {
+  https.createServer(options, app).listen(4005, () => {
     console.log('Servidor HTTPS escuchando en el puerto 4006');
   });
 
